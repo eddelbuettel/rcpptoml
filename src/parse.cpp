cd317254cc3d34226a4e80682dcbbc8cb3fa37ca
Dirk Eddelbuettel <edd@debian.org>
2015-03-29 15:55:38 -0700

    recognises value types and prints them

--- src/parse.cpp
+++ src/parse.cpp
@@ -3,6 +3,38 @@
 
 #include <Rcpp.h>
 
+std::string escapeString(const std::string& str) {
+    std::string res;
+    for (auto it = str.begin(); it != str.end(); ++it) {
+        if (*it == '\\')
+            res += "\\\\";
+        else if (*it == '"')
+            res += "\\\"";
+        else if (*it == '\n')
+            res += "\\n";
+        else
+            res += *it;
+    }
+    return res;
+}
+
+void printValue(std::ostream& o, const std::shared_ptr<cpptoml::base>& base) {
+    if (auto v = base->as<std::string>()) {
+        o << "{\"type\":\"string\",\"value\":\"" << escapeString(v->get())
+          << "\"}";
+    } else if (auto v = base->as<int64_t>()) {
+        o << "{\"type\":\"integer\",\"value\":\"" << v->get() << "\"}";
+    } else if (auto v = base->as<double>()) {
+        o << "{\"type\":\"float\",\"value\":\"" << v->get() << "\"}";
+    } else if (auto v = base->as<cpptoml::datetime>()) {
+        o << "{\"type\":\"datetime\",\"value\":\"" << v->get() << "\"}";
+    } else if (auto v = base->as<bool>()) {
+        o << "{\"type\":\"bool\",\"value\":\"";
+        v->print(o);
+        o << "\"}";
+    }
+}
+
 // [[Rcpp::export]]
 Rcpp::List tomlparse(std::string filename) {
 
@@ -28,6 +60,14 @@ Rcpp::List tomlparse(std::string filename) {
         } else if (p.second->is_array()) {
             Rcpp::Rcout << "Array: " << p.first << std::endl;
             sl.push_front(p.first); 
+        } else if (p.second->is_value()) {
+            //std::shared_ptr<cpptoml::base> bp = p.second;
+            Rcpp::Rcout << "Value: " << p.first 
+                        << "\n  :";
+            printValue(std::cout, p.second);
+            Rcpp::Rcout << std::endl;
+            sl.push_front(p.first); 
+            
         } else {
             Rcpp::Rcout << "Other: " << p.first << std::endl;
             sl.push_front(p.first); 
