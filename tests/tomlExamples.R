f9648dde243004a8af694cfb3b98b1c8e9176d99
Dirk Eddelbuettel <edd@debian.org>
2015-04-04 09:10:05 -0500

    added toml v0.4.0 spec example
    NB: line 56 edited as the cpptoml parser cannot deal with the wide char

--- tests/tomlExamples.R
+++ tests/tomlExamples.R
@@ -1,6 +1,13 @@
 
 library(RcppToml)
 
+## basic toml-lang::tests/example.toml
 print(str(tomlparse("toml_example.toml")))
 
+## harder toml-lang::tests/hard_example.toml
 print(str(tomlparse("toml_hard_example.toml")))
+
+## example toml-lang::examples/example-v0.4.0.toml
+##
+## note that we edited line 56 which cannot be parsed by cpptoml
+print(str(tomlparse("toml_example-v0.4.0.toml")))
