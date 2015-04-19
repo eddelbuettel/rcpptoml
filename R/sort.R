42d78c6469a177728010609ea8935d8703b846ee
Dirk Eddelbuettel <edd@debian.org>
2015-04-19 18:10:49 -0500

    added new (recursive) sort helper

---
+++ R/sort.R
@@ -0,0 +1,14 @@
+
+.sort <- function(s) {
+    if (length(s) > 0) {
+        for (elem in seq_along(s)) {
+            if (is.list(s[[elem]])) {
+                s[[elem]] <- .sort(s[[elem]])
+            }
+        }
+        if (length(names(s)) > 0) {
+            s <- s[order(names(s))]
+        }
+    }
+    s
+}
